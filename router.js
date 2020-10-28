ec38fd06adfaaa19838ee147af4db30e9b8ce238
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2020-10-28 15:49:51 -0600

    Add endpoint for a new attendee

--- router.js
+++ router.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const {addAttendee, createMeeting} = require('./service');
+
 router.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-router.post('/attendees', (req, res) => {
-  res.json({result: 'Attendee'});
+router.post('/attendees', async (req, res) => {
+  const {meetingId} = req.body;
+  const newAttendee = await addAttendee(meetingId);
+  res.json({attendee: {id: newAttendee.Attendee.AttendeeId}});
 });
 
-router.post('/meetings', (req, res) => {
-  res.json({result: true});
+router.post('/meetings', async (req, res) => {
+  const newMeeting = await createMeeting();
+  res.json({meeting: {id: newMeeting.Meeting.MeetingId}});
 });
 
 module.exports = router;
--- service.js
+++ service.js
@@ -1,14 +1,21 @@
 const {v4: uuidv4} = require('uuid');
-const {addAttendee, createMeeting} = require('./chime-methods');
+const {
+  addAttendee: chimeAddAttendee,
+  createMeeting: chimeCreateMeeting,
+} = require('./chime-methods');
 
-const run = async () => {
+async function createMeeting() {
   const requestId = uuidv4();
-  const meeting = await createMeeting(requestId);
-  const meetingId = meeting.Meeting.MeetingId;
+  const meeting = await chimeCreateMeeting(requestId);
+  //const meetingId = meeting.Meeting.MeetingId;
+  return meeting;
+}
 
+async function addAttendee(meetingId) {
   const externalUserId = uuidv4();
-  const attendee = await addAttendee(meetingId, externalUserId);
-  const attendeeId = attendee.Attendee.AttendeeId;
-};
+  const attendee = await chimeAddAttendee(meetingId, externalUserId);
+  //const attendeeId = attendee.Attendee.AttendeeId;
+  return attendee;
+}
 
-run();
+module.exports = {addAttendee, createMeeting};
